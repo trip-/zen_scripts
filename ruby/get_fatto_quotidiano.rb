a116ef9dc646a888ee8ea2f7f6a215d21addedfe
Enrico Carlesso <enrico@ecarlesso.org>
2010-07-25 23:45:16 +0200

    Moved check for monday before logging, and moved sensible data in ~/.zen_data

--- ruby/get_fatto_quotidiano.rb
+++ ruby/get_fatto_quotidiano.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+# -*- coding: utf-8 -*-
 #
 # Author: Mattia Gheda
 # Title: get_fatto_quotidiano
@@ -12,7 +13,7 @@
 
 # Configuration:
 # You need to have a valid user on fatto's website 
-# FATTOUSER,FATTOPSW, DPATH have to be set
+# FATTOUSER, FATTOPSW, DPATH have to be set
 
 # License: see LICENSE 
 # Simple script to download today's copy of FattoQuotidiano
@@ -20,15 +21,25 @@
 require 'rubygems'
 
 begin
-    require 'mechanize'
-    rescue LoadError
-        $stderr.print "#{File.basename(__FILE__)} requires mechanize gem to work\nPlease install it with gem install mechanize\n"
-    exit
+  require 'mechanize'
+rescue LoadError
+  $stderr.print "#{File.basename(__FILE__)} requires mechanize gem to work\nPlease install it with gem install mechanize\n"
+  exit
 end
+begin
+  require 'parseconfig'
+rescue LoadError
+  $stderr.print "#{File.basename(__FILE__)} requires parseconfig gem to work\nPlease install it with gem install parseconfig\n"
+  exit
+end  
 
-# Il fatto website account data
-FATTOUSER="thamayor@gmail.com"
-FATTOPSW="condiviso"
+# Il fatto website account data strored in ~/.zen_data
+# in the form:
+# FATTOUSER=''
+# FATTOPWD=''
+zen_config = ParseConfig.new(File.expand_path('~/.zen_data'))
+FATTOUSER=zen_config.get_value('FATTOUSER')
+FATTOPSW=zen_config.get_value('FATTOPWD')
 
 # Download folder
 DPATH = "./"
@@ -38,21 +49,18 @@ agent = Mechanize.new
 page = agent.get 'http://www.ilfattoquotidiano.it/abbonati/'
 login_page = agent.click(page.link_with(:href => /login/))
 
-my_page = login_page.form_with(:name => "loginform") do |f|
-    f.log = FATTOUSER
-    f.pwd = FATTOPSW
-end.click_button
-
 now = DateTime.now
 if now.wday == 1 #Monday
-    puts "oggi niente fatto"
-    exit 0
+  puts "Il Lunedì niente fatto"
+  exit 0
 else
-    today = sprintf("%d%02d%02d",now.year,now.month,now.day)
-    pdf = agent.get "http://www.ilfattoquotidiano.it/openpdf/?n=#{today}"
-    File.open("#{File.join(DPATH,today)}.pdf", "w") do |file|
-        file << pdf.body
-    end
+  my_page = login_page.form_with(:name => "loginform") do |f|
+    f.log = FATTOUSER
+    f.pwd = FATTOPSW
+  end.click_button
+  today = sprintf("%d%02d%02d",now.year,now.month,now.day)
+  pdf = agent.get "http://www.ilfattoquotidiano.it/openpdf/?n=#{today}"
+  File.open("#{File.join(DPATH,today)}.pdf", "w") do |file|
+    file << pdf.body
+  end
 end
-
-
